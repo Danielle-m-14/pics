e7dfe701a57f1f5f08efc6bb64959061a10e7235
Danielle_M <daniellematalonis@gmail.com>
2021-04-16 23:16:38 -0400

    Changed App to class component and added onSubmit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,18 @@ import React from "react";
 
 class SearchBar extends React.Component {
 
-state = { term: "" };
+  state = { term: "" };
 
-onFormSubmit(event) {
-  event.preventDefault();
+  onFormSubmit = event => {
+    event.preventDefault();
 
-  console.log(this.state.term);
-}
+    this.props.onSubmit(this.state.term);
+  };
 
     render() {
         return (
           <div className="ui segment">
-            <form onSubmit={this.onFormSubmit} clasName="ui form">
+            <form onSubmit={this.onFormSubmit} className="ui form">
                 <div className="field">
                 <label>Image Search</label>
               <input 
