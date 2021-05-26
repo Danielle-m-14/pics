af12d6c10d62e067bf61a9503edae7450bf1fd78
Danielle_M <daniellematalonis@gmail.com>
2021-05-26 19:46:56 -0400

    refining where to get results

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,15 @@ import SearchBar from "./SearchBar";
 
 class App extends React.Component {
 
-onSearchSubmit(term) {
-  axios.get("https://api.unsplash.com/search/photos", {
+async onSearchSubmit(term) {
+  const response = await axios
+  .get("https://api.unsplash.com/search/photos", {
     params: { query: term },
     headers: {
       Authorization: "Client-ID HbnU7C1KYwCEu2HSs2zapffaWi7gfGMwRpGo-valZ_U"
     }
   });
+  console.log(response.data.results);
 }
 
   render() {  
